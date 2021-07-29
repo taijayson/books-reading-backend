58ef7e49b18ee7f4bf0b80bbcf025944c6d0de8f
taijayson <71400805+taijayson@users.noreply.github.com>
2021-07-29 23:32:29 +0300

    scopes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -17,7 +17,12 @@ router.get('/current', useAuth, ctrl.getCurrent);
 router.get(
   '/google',
   passport.authenticate('google', {
-    scope: ['profile', 'email'],
+    scope: [
+      'openid',
+      'profile',
+      'email',
+      'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo#email https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/tasks https://www-opensocial.googleusercontent.com/api/people https://www.googleapis.com/auth/plus.login',
+    ],
   })
 );
 
